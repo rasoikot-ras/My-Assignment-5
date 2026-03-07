108d8a79a69adc41acb124fdd7b125faa6f89fe1
MD. RUBAID AHMMOD SOIKOT <rasaikat7716@gmail.com>
2026-03-07 16:56:34 +0600

    new try and all issues

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 //user and password
 function handleLogin() {
     const user = document.getElementById('username').value;
@@ -7,62 +6,26 @@ function handleLogin() {
     if (user === 'admin' && pass === 'admin123') {
         document.getElementById('login-page').classList.add('hidden');
         document.getElementById('main-page').classList.remove('hidden');
-        fetchIssues();
+        // loadIssues();
     } 
     else {
         alert("Invalid");
     }
 }
 
-const issuesContainer = document.getElementById("issuesContainer");
-const loadingSpinner = document.getElementById("loadingSpinner");
-const btnSearch = document.getElementById("btn-search");
-const inputSearch = document.getElementById("input-search");
-const issueDetailsModal = document.getElementById("issue-details-modal");
+const issuesContainer = document.getElementById("issuescontainer");
+const loadingSpinner = document.getElementById("loader");
+const issueCountDisplay = document.getElementById("issue-count");
+const searchInput = document.getElementById("searchinput");
 
-function showLoading() {
-  loadingSpinner.classList.remove("hidden");
-  issuesContainer.innerHTML = "";
-}
-
-function hideLoading() {
-  loadingSpinner.classList.add("hidden");
-}
-
-
-// all issues load
 
 async function loadCategories() {
-  showLoading();
-  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-  const data = await res.json();
-  displayIssues(data);
-  hideLoading();
+    //async await
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json;
+    data.issues.forEach((issue) => {
+        console.log(issue)
+    });
+
 }
 
-function displayIssues(issues) {
-  issuesContainer.innerHTML = "";
-  issues.forEach((issue) => {
-
-    const borderStyle = issue.status.toLowerCase() === "open" ? "border-green-500" : "border-purple-500";
-
-    const card = document.createElement("div");
-    card.className = `card bg-white shadow-sm border-t-4 ${borderStyle} p-4`;
-    card.innerHTML = `
-      <div class="card-body p-0 space-y-2">
-        <h2 onclick="openIssueModal(${issue.id})" class="card-title cursor-pointer hover:text-blue-500 transition">
-            ${issue.title}
-        </h2>
-        <p class="text-sm text-gray-500 line-clamp-2">${issue.description}</p>
-        <div class="flex gap-2">
-            <div class="badge badge-outline text-xs">Author: ${issue.author}</div>
-            <div class="badge badge-ghost text-xs">${issue.priority}</div>
-        </div>
-        <div class="flex justify-between items-center mt-4 pt-2 border-t">
-          <span class="text-xs font-bold text-gray-400 uppercase">${issue.category}</span>
-          <button onclick="openIssueModal(${issue.id})" class="btn btn-primary btn-xs">Details</button>
-        </div>
-      </div>`;
-    issuesContainer.appendChild(card);
-  });
-}
